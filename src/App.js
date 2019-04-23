3a762631acb3d0decb6ac0ba5b59220f23fbeb40
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-23 12:00:39 +0100

    Add components to base app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import ArticleList from './components/ArticleList';
+import { Router } from '@reach/router';
+import * as api from './utils/api';
 
-function App() {
-  return <div className="App">App</div>;
+class App extends Component {
+  state = {
+    topics: [],
+  };
+
+  render() {
+    const { topics } = this.state;
+    return (
+      <div className="App">
+        <Header />
+        <Nav topics={topics} />
+        <Router className="ArticleContainer">
+          <ArticleList path="/" />
+          <ArticleList path="/topics/:topic_id" />
+        </Router>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.fetchTopics();
+  }
+
+  fetchTopics = () => {
+    api.getTopics().then(newTopics => {
+      this.setState({ topics: newTopics });
+    });
+  };
 }
 
 export default App;
