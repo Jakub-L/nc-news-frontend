720916c7ee7956a552566e3d1ddfca9f43ca2dd5
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-25 16:56:11 +0100

    Add basicUrl to cypress config

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,18 +1,20 @@
+const BASE_URL = Cypress.config().baseUrl;
+
 describe('Auth', () => {
   it('Username input contains default username and allows logging in with it', () => {
-    cy.visit('localhost:3000/auth');
+    cy.visit('/auth');
     cy.get('input#username').should('have.value', 'tickle122');
     cy.get('button')
       .contains('Log in')
       .click();
-    cy.url().should('equal', 'http://localhost:3000/');
+    cy.url().should('equal', `${BASE_URL}/`);
   });
   it('Allows logging in with a non-default, but valid username', () => {
-    cy.visit('localhost:3000/auth');
+    cy.visit('/auth');
     cy.get('input#username').type('jessjelly');
     cy.get('button')
       .contains('Log in')
       .click();
-    cy.url().should('equal', 'http://localhost:3000/');
+    cy.url().should('equal', `${BASE_URL}/`);
   });
 });
