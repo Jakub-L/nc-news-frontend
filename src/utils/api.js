25aebfc6c6112400e5d9dde26d0d78d8e0d24776
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-25 16:13:31 +0100

    Change API calls to be over HTTPS due to deployment issue on Netlify

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://nc-news-jakub.herokuapp.com/api';
+const BASE_URL = 'https://nc-news-jakub.herokuapp.com/api';
 
 export const getTopics = async () => {
   const { data } = await axios.get(`${BASE_URL}/topics`);
