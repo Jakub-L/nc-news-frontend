851470508140736e735818ee89aa2d194d9f9940
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-26 12:26:29 +0100

    Add feedback when trying to submit empty comments

--- src/components/CommentSubmit.jsx
+++ src/components/CommentSubmit.jsx
@@ -4,34 +4,46 @@ import '../styles/CommentSubmit.css';
 class CommentSubmit extends Component {
   state = {
     comment: '',
+    submitFail: false,
   };
 
   render() {
-    const { comment } = this.state;
+    const { comment, submitFail } = this.state;
     return (
       <form className="CommentSubmit" onSubmit={this.handleSubmit}>
         <textarea
           maxLength="5000"
           rows="10"
           id="comment"
+          className={submitFail ? 'submit-fail' : null}
+          placeholder={submitFail ? 'You cannot submit empty comments' : null}
           value={comment}
           onChange={this.handleChange}
+          onFocus={this.handleFocus}
         />
         <button type="submit">Add Comment</button>
       </form>
     );
   }
 
+  handleFocus = () => {
+    this.setState({ submitFail: false });
+  };
+
   handleChange = event => {
     const { id, value } = event.target;
-    this.setState({ [id]: value });
+    this.setState({ [id]: value, submitFail: false });
   };
 
   handleSubmit = event => {
     event.preventDefault();
     const { value } = event.target.comment;
-    this.props.addComment(value);
-    this.setState({ comment: '' });
+    if (value.length) {
+      // this.props.addComment(value);
+      this.setState({ comment: '' });
+    } else {
+      this.setState({ submitFail: true });
+    }
   };
 }
 
