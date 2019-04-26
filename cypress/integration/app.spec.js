10de516d6326d54c19b80df944bc2bd112ec7140
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-26 10:36:01 +0100

    Add testing of basic ArticleList display

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,4 +1,60 @@
 const BASE_URL = Cypress.config().baseUrl;
+describe('Home', () => {
+  beforeEach(() => {
+    cy.clearSessionStorage();
+    cy.stub();
+    cy.visit('/');
+  });
+  it('Loads articles into a list, sorted by created_at by default', () => {
+    cy.get('.ArticleCard').should('have.length', 10);
+    cy.get('.ArticleCard > .article-time')
+      .first()
+      .invoke('text')
+      .should('equal', 'Posted on 2018-05-30 at 15:59');
+    cy.get('.ArticleCard > .article-time')
+      .last()
+      .invoke('text')
+      .should('equal', 'Posted on 2017-11-05 at 07:22');
+  });
+  it('Allows sorting by different categories', () => {
+    cy.get('.sort-dropdown').select('comment_count');
+    cy.get('.ArticleCard > .article-title')
+      .first()
+      .invoke('text')
+      .should('equal', 'History of FC Barcelona');
+    cy.get('.ArticleCard > .article-title')
+      .last()
+      .invoke('text')
+      .should('equal', 'HOW COOKING HAS CHANGED US');
+    cy.get('.sort-dropdown').select('votes');
+    cy.get('.ArticleCard > .article-title')
+      .first()
+      .invoke('text')
+      .should('equal', 'Seafood substitutions are increasing');
+    cy.get('.ArticleCard > .article-title')
+      .last()
+      .invoke('text')
+      .should(
+        'equal',
+        'Why do England managers keep making the same mistakes?'
+      );
+  });
+  it('Allows sorting ascending/descending', () => {
+    cy.get('.sort-dropdown').select('votes');
+    cy.get('.sort-button').click();
+    cy.get('.ArticleCard > .article-title')
+      .first()
+      .invoke('text')
+      .should('equal', 'High Altitude Cooking');
+    cy.get('.ArticleCard > .article-title')
+      .last()
+      .invoke('text')
+      .should(
+        'equal',
+        'Why do England managers keep making the same mistakes?'
+      );
+  });
+});
 
 describe('Auth', () => {
   beforeEach(() => {
@@ -28,12 +84,12 @@ describe('Auth', () => {
       .should('have.value', 'tickle122');
   });
   it('Takes user to the previous page they were on after login', () => {
-    cy.visit('/articles/10');
+    cy.visit('/articles/33');
     cy.get('[data-cy=login-link]').click();
     cy.get('button')
       .contains('Log in')
       .click();
-    cy.url().should('equal', `${BASE_URL}/articles/10`);
+    cy.url().should('equal', `${BASE_URL}/articles/33`);
   });
   it('Fails login with invalid username and displays notification', () => {
     cy.get('input#username').type('invalid');
