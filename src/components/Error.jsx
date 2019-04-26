927599c86e20c04be1059f1c69538f34be290b48
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-26 12:55:34 +0100

    Add more messages for various errors

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { navigate } from '@reach/router';
 import '../styles/Error.css';
 
-const Error = () => {
+const Error = ({ uri }) => {
+  const messages = {
+    '/error/400': 'Something went wrong when making your request.',
+    '/error/404': "We couldn't find what you were looking for.",
+    '/error/405': 'Your browser tried to use a method we do not support.',
+    '/error/422': 'Your request could not be processed.',
+    '/error/500': "There's an error with our server! We're working on it.",
+  };
   return (
     <div className="Error">
       <div id="error-icon">
@@ -10,8 +17,12 @@ const Error = () => {
       </div>
       <div id="error-message">
         <h2 className="heading">Oops! Something went wrong!</h2>
-        <p className="body">We couldn't find what you were looking for.</p>
-        <button onClick={() => navigate('/')}>Go back!</button>
+        <p className="body">
+          {messages[uri]
+            ? messages[uri]
+            : "We couldn't find what you were looking for."}
+        </p>
+        <button onClick={() => navigate('/')}>Get me Home!</button>
       </div>
     </div>
   );
