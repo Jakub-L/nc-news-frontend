3b33f186fa48c00ca4d076db7a33c380b455b841
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-26 10:35:48 +0100

    Add routes to stubbing server

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,6 +5,7 @@ Cypress.Commands.add('clearSessionStorage', () => {
 Cypress.Commands.add('stub', () => {
   cy.server();
   cy.route('*/topics', 'fx:topics.json');
+  // User routes
   cy.route('*/users/tickle122', 'fx:users-tickle122.json');
   cy.route('*/users/jessjelly', 'fx:users-jessjelly.json');
   cy.route({
@@ -12,4 +13,35 @@ Cypress.Commands.add('stub', () => {
     status: 404,
     response: 'fx:users-invalid.json',
   });
+  // Article lists routes
+  cy.route(
+    '*/articles?sort_by=created_at&order=desc',
+    'fx:articles-all-createdat-desc.json'
+  );
+  cy.route(
+    '*/articles?sort_by=comment_count&order=desc',
+    'fx:articles-all-commentcount-desc.json'
+  );
+  cy.route(
+    '*/articles?sort_by=votes&order=desc',
+    'fx:articles-all-votes-desc.json'
+  );
+  cy.route(
+    '*/articles?sort_by=votes&order=asc',
+    'fx:articles-all-votes-asc.json'
+  );
+  cy.route(
+    '*/articles?sort_by=created_at&order=desc&topic=football',
+    'fx:articles-football-createdat-desc.json'
+  );
+  cy.route(
+    '*/articles?sort_by=created_at&order=desc&topic=invalid',
+    'fx:articles-invalid.json'
+  );
+
+  // Individual article routes
+  cy.route('*/articles/33', 'fx:article-33-article');
+  cy.route('*/articles/33/comments', 'fx:article-33-comments');
+  cy.route('*/articles/99999', 'fx:article-invalid-article');
+  cy.route('*/articles/99999/comments', 'fx:article-invalid-comments');
 });
